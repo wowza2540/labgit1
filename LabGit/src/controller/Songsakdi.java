830d793ab64bd0e9919f70d42832c6eb47390978
Inside <Inside@172.25.99.140>
2017-11-10 11:47:55 +0700

    code เริ่มต้นหลังเสร็จsprint1(LabGit)

---
+++ LabGit/src/controller/Songsakdi.java
@@ -0,0 +1,10 @@
+package controller;
+
+import javax.swing.JOptionPane;
+
+public class Songsakdi {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Welcome to Github Lab.");
+	}
+
+}
